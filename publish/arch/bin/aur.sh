cedb5eefce2db9904e992149cbfedd747f5d0b3c
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2020-03-27 04:29:04 +0200

    chore(pipeline): debug container permissions

--- publish/arch/bin/aur.sh
+++ publish/arch/bin/aur.sh
@@ -2,16 +2,23 @@
 
 set -e
 
-CURRENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-PARENT_PATH=$(cd "${CURRENT_PATH}/../"; pwd -P)
+CURRENT_PATH=$(
+	cd "$(dirname "${BASH_SOURCE[0]}")"
+	pwd -P
+)
+PARENT_PATH=$(
+	cd "${CURRENT_PATH}/../"
+	pwd -P
+)
 REPO="dnsadblock/proxy-release"
 
-main(){
+main() {
 	GIT_TAG=$(get_latest_release)
 	GIT_TAG=$(remove_vversion "$GIT_TAG")
 	echo "Creating package for v${GIT_TAG}"
 
 	echo "Cloning AUR repo"
+	rm -rf "$PARENT_PATH/dnsadblock"
 	git clone git+ssh://aur@aur.archlinux.org/dnsadblock.git "${PARENT_PATH}/dnsadblock"
 
 	PKGBUILD_TPL=${PKGBUILD_TPL/__PACKAGE_VERSION__/$GIT_TAG}
@@ -40,21 +47,27 @@ package() {
 EOL
 
 	echo "Building base image"
-	docker build -t dnsadblock/arch $PARENT_PATH
+	docker build -t dnsadblock/arch \
+		--build-arg USER_ID=$(id -u) \
+		--build-arg GROUP_ID=$(id -g) \
+		$PARENT_PATH
 
 	echo "Creating PKGBUILD"
-	docker run -v "$PARENT_PATH/dnsadblock":/home/non_root/dnsadblock dnsadblock/arch makepkg -g -f -p PKGBUILD
+	docker run \
+		-v "$PARENT_PATH/dnsadblock":/home/non_root/dnsadblock dnsadblock/arch \
+		makepkg -g -f -p PKGBUILD
 
 	echo "Generating sha256sums"
 	add_sha256sums
 
 	echo "Creating .SRCINFO"
-	docker run -v "$PARENT_PATH/dnsadblock":/home/non_root/dnsadblock dnsadblock/arch makepkg --printsrcinfo > "$PARENT_PATH/dnsadblock/.SRCINFO"
+	docker run -v "$PARENT_PATH/dnsadblock":/home/non_root/dnsadblock dnsadblock/arch makepkg --printsrcinfo >"$PARENT_PATH/dnsadblock/.SRCINFO"
 
 	cd "${PARENT_PATH}/dnsadblock"
 
 	# do not commit unless we have the required files present
 	[ -f "$PARENT_PATH/dnsadblock/.SRCINFO" ] && [ -f "$PARENT_PATH/dnsadblock/PKGBUILD" ] && {
+		echo "Required files are present; will push to origin now"
 		git commit -am "Release for version: v${GIT_TAG}"
 		git push origin master
 	}
@@ -70,19 +83,19 @@ remove_vversion() {
 	echo "$1" | cut -c2-9999
 }
 
-add_sha256sums(){
+add_sha256sums() {
 	shasum=$(sha256sum "$PARENT_PATH/dnsadblock/dnsadblock_${GIT_TAG}_linux_amd64.tar.gz" | cut -f1 -d' ')
 	shasum=$(trim "$shasum")
-	echo "sha256sums=('${shasum}')" >> "$PARENT_PATH/dnsadblock/PKGBUILD"
+	echo "sha256sums=('${shasum}')" >>"$PARENT_PATH/dnsadblock/PKGBUILD"
 }
 
 trim() {
-    local var="$*"
-    # remove leading whitespace characters
-    var="${var#"${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
-    var="${var%"${var##*[![:space:]]}"}"
-    printf '%s' "$var"
+	local var="$*"
+	# remove leading whitespace characters
+	var="${var#"${var%%[![:space:]]*}"}"
+	# remove trailing whitespace characters
+	var="${var%"${var##*[![:space:]]}"}"
+	printf '%s' "$var"
 }
 
 main
