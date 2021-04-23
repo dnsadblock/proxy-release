99dcc39affd9c383cc5f035e51075733ac6fec4a
Mihalcea Romeo <romeo.mihalcea@gmail.com>
2021-04-23 19:55:15 +0300

    fix: directory not created with root permission on bin install mode

--- install.sh
+++ install.sh
@@ -193,7 +193,7 @@ install_bin() {
 	log_debug "Installing $LATEST_RELEASE binary for $GOOS/$GOARCH to $bin_path"
 	url="https://github.com/dnsadblock/proxy-release/releases/download/v${LATEST_RELEASE}/dnsadblock_${LATEST_RELEASE}_${GOOS}_${GOARCH}.tar.gz"
 	log_debug "Downloading $url"
-	mkdir -p "$(dirname "$bin_path")" &&
+	asroot mkdir -p "$(dirname "$bin_path")" &&
 		curl -sL "$url" | asroot sh -c "tar Ozxf - dnsadblock > \"$bin_path\"" &&
 		asroot chmod 755 "$bin_path"
 }
