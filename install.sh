639e75973fa6677c0fd63e42aa5e806b519688cc
DnsAdBlock <office@dnsadblock.com>
2021-07-01 12:55:07 +0300

    upstream: Fix install script get last release on some platforms

--- install.sh
+++ install.sh
@@ -1014,8 +1014,8 @@ get_release() {
 			return
 			;;
 		esac
-		out=$($cmd "https://api.github.com/repos/dnsadblock/proxy-release/releases/latest")
-		v=$(echo "$out" | grep '"tag_name":' | esed 's/.*"([^"]+)".*/\1/' | sed -e 's/^v//')
+		v=$($cmd "https://api.github.com/repos/dnsadblock/proxy-release/releases/latest" | \
+            grep '"tag_name":' | esed 's/.*"([^"]+)".*/\1/' | sed -e 's/^v//')
 		if [ -z "$v" ]; then
 			log_error "Cannot get latest version: $out"
 		fi
